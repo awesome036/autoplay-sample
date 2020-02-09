06065224d0709fdad50cad7beb86c5e0d3aad647
OsamuKurosawa <hillzen.asia@gmail.com>
2020-02-09 16:04:36 +0900

    first commit

---
+++ src/components/page1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>ここはページ１です。</h1>
+    <router-link to="/page2">Go to page2</router-link>
+    <div>
+      <audio
+        id="player1"
+        autoplay
+        controls
+        src="/static/audio/kanpai.mp3"
+        type="audio/mpeg"
+      ></audio>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+let init = () => {
+  document.addEventListener("click", audioPlay);
+};
+
+let audioPlay = () => {
+  let player = document.getElementById("player1");
+  player.play();
+  document.removeEventListener("click", audioPlay);
+};
+
+init();
+</script>
+
+<style></style>
---
+++ src/components/page2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>ここはページ２です。</h1>
+    <router-link to="/">Go to page1</router-link>
+    <div>
+      <audio
+        id="player2"
+        autoplay
+        controls
+        src="/static/audio/kanpai.mp3"
+        type="audio/mpeg"
+      ></audio>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
